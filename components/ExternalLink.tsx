9fb63989fe9f1401213d1ab64b84571757bf1b68
shakib1729 <shakibahmed0786@gmail.com>
2025-04-30 14:51:43 +0530

    style: update styles for ExternalLink

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,6 +1,6 @@
 const ExternalLink = ({ href, title }: { href: string; title: string }) => {
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer">
+    <a href={href} target="_blank" rel="noopener noreferrer" className="underline font-medium text-slate-900">
       {title}
     </a>
   );
